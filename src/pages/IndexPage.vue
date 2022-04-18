b76c731e1cf34e8e1d44a15f72a79e42842fe954
Tate Mitchell <htatemitchell@gmail.com>
2022-04-18 15:24:37 -0600

    initial push

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <q-page class="">
+    <h1>Home</h1>
+    <h2>Featured Locations</h2>
+    <h2>Upcoming Events</h2>
+  </q-page>
+</template>
+
+<style scoped>
+</style>
