7c15dc72152b795372d317e3ac3234a24c42d882
Tate Mitchell <htatemitchell@gmail.com>
2022-04-18 16:35:47 -0600

    Issue #2 created blank page placeholders

---
+++ src/pages/SearchPage.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+    <q-page class="">
+        <h1>Search</h1>
+        <input>
+        <button>Search</button>
+    </q-page>
+</template>
+
+<style scoped>
+</style>
---
+++ src/pages/StoreDetailsPage.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+    <q-page class="">
+        <h1>Store Details</h1>
+    </q-page>
+</template>
+
+<style scoped>
+</style>
