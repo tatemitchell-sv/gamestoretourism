e207b98eb114760963400ec05a38ffc48023eecd
Tate Mitchell <htatemitchell@gmail.com>
2022-04-19 18:10:35 -0600

    Issue #4 started masonry gallery for store details page

---
+++ src/components/MasonryGallery.vue
@@ -0,0 +1,78 @@
+<script setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {
+    var elem = document.querySelector('.grid');
+    if (elem) {
+        var msnry = new Masonry(elem, {
+            // options
+            itemSelector: '.grid-item',
+            columnWidth: 200
+        });
+        var msnry = new Masonry('.grid', {
+            //options
+        });
+    }
+
+    console.log('element is: ' + elem)
+})
+
+</script>
+
+<template>
+    <div class="grid">
+        <div class="grid-item">
+            <img
+                src="https://res.cloudinary.com/htatemitchell/image/upload/h_350,w_200,c_fill/v1650404500/gamestoretour/madnessthumbnail_smlvw9.jpg" />
+        </div>
+        <div class="grid-item">
+            <img
+                src="https://res.cloudinary.com/htatemitchell/image/upload/h_100,w_200,c_fill/v1650404500/gamestoretour/madnessthumbnail_smlvw9.jpg" />
+        </div>
+        <div class="grid-item">
+            <img
+                src="https://res.cloudinary.com/htatemitchell/image/upload/h_400,w_200,c_fill/v1650404500/gamestoretour/madnessthumbnail_smlvw9.jpg" />
+        </div>
+        <div class="grid-item">
+            <img
+                src="https://res.cloudinary.com/htatemitchell/image/upload/h_1000,w_200,c_fill/v1650404500/gamestoretour/madnessthumbnail_smlvw9.jpg" />
+        </div>
+        <div class="grid-item">
+            <img
+                src="https://res.cloudinary.com/htatemitchell/image/upload/h_800,w_200,c_fill/v1650404500/gamestoretour/madnessthumbnail_smlvw9.jpg" />
+        </div>
+        <div class="grid-item">
+            <img
+                src="https://res.cloudinary.com/htatemitchell/image/upload/h_100,w_200,c_fill/v1650404500/gamestoretour/madnessthumbnail_smlvw9.jpg" />
+        </div>
+        <div class="grid-item">
+            <img
+                src="https://res.cloudinary.com/htatemitchell/image/upload/h_600,w_200,c_fill/v1650404500/gamestoretour/madnessthumbnail_smlvw9.jpg" />
+        </div>
+        <div class="grid-item">
+            <img
+                src="https://res.cloudinary.com/htatemitchell/image/upload/h_350,w_200,c_fill/v1650404500/gamestoretour/madnessthumbnail_smlvw9.jpg" />
+        </div>
+        <div class="grid-item">
+            <img
+                src="https://res.cloudinary.com/htatemitchell/image/upload/h_500,w_200,c_fill/v1650404500/gamestoretour/madnessthumbnail_smlvw9.jpg" />
+        </div>
+    </div>
+
+</template>
+
+<style scoped>
+.grid-item img {
+    width: 100%;
+    max-width: 200px;
+    display: block;
+}
+
+.grid-item {
+    width: 200px;
+}
+
+.grid-item--width2 {
+    width: 400px;
+}
+</style>
--- src/pages/StoreDetailsPage.vue
+++ src/pages/StoreDetailsPage.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { useRoute } from 'vue-router';
 import data from '../../gamestores.js'
-
+import MasonryGallery from 'src/components/MasonryGallery.vue';
 const route = useRoute()
 const storeID = route.params.id;
 const store = data[storeID];
@@ -11,6 +11,8 @@ const store = data[storeID];
     <q-page class="">
         <h1>Store Details</h1>
         <h2>{{ store.name }}</h2>
+        <MasonryGallery />
+
     </q-page>
 </template>
 
