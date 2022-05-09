cf3ed6da6afb5a1c7fb5518bf797f524e5bac322
Tate Mitchell <htatemitchell@gmail.com>
2022-05-09 10:26:50 -0600

    pre deploy

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:tm437647/gamestoretourism.git master:gh-pages
+git push -f git@github.com:tm437647/gamestoretourism.git main:gh-pages
 cd -
