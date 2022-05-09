425c4133f790779440d38416f341b145e0ba59d8
Tate Mitchell <htatemitchell@gmail.com>
2022-05-09 09:55:25 -0600

    pre deploy steps

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/gamestorefinder/" : "/",
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/gamestoretourism/" : "/",
 };
