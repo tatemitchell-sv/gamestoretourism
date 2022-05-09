eaeb2918fdba792c4945b246d69f0d94ea2f84bd
Tate Mitchell <htatemitchell@gmail.com>
2022-05-09 11:45:37 -0600

    deploy

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,98 +1,104 @@
 import axios from "axios";
 
+const liveAPIUrl = "https://gamestoretourism437647.herokuapp.com";
+const localAPIUrl = "http://localhost:8081";
+
+// set baseUrl to one of the above links for testing or live deployment
+const baseUrl = liveAPIUrl;
+
 export default {
   getAllStores: () => {
-    return axios.get("http://localhost:8081/");
+    return axios.get(`${baseUrl}/`);
   },
 
   getFeaturedStores: () => {
-    return axios.get("http://localhost:8081/featuredstores");
+    return axios.get(`${baseUrl}/featuredstores`);
   },
 
   getStoreById: (id) => {
-    return axios.get(`http://localhost:8081/store/${id}`);
+    return axios.get(`${baseUrl}/store/${id}`);
   },
 
   searchStore: (searchString) => {
-    return axios.post("http://localhost:8081/simplesearch", {
+    return axios.post(`${baseUrl}/simplesearch`, {
       searchString: searchString,
     });
   },
 
   advSearch: (searchCriteria) => {
-    return axios.post("http://localhost:8081/advancedsearch", {
+    return axios.post(`${baseUrl}/advancedsearch`, {
       ...searchCriteria,
     });
   },
 
   createStore: (newStore) => {
-    return axios.post("http://localhost:8081/createstore", {
+    return axios.post(`${baseUrl}/createstore`, {
       newStore,
     });
   },
 
   createEvent: (newEvent, storeID) => {
-    return axios.post("http://localhost:8081/createevent", {
+    return axios.post(`${baseUrl}/createevent`, {
       newEvent,
       storeID,
     });
   },
 
   createProduct: (newProduct, storeID) => {
-    return axios.post("http://localhost:8081/createproduct", {
+    return axios.post(`${baseUrl}/createproduct`, {
       newProduct,
       storeID,
     });
   },
 
   createImage: (newImage, storeID) => {
-    return axios.post("http://localhost:8081/createimage", {
+    return axios.post(`${baseUrl}/createimage`, {
       newImage,
       storeID,
     });
   },
 
   editStore: (edittedStore, storeID) => {
-    return axios.put("http://localhost:8081/editstore", {
+    return axios.put(`${baseUrl}/editstore`, {
       edittedStore,
       storeID,
     });
   },
 
   editEvent: (edittedEvent, storeID) => {
-    return axios.put("http://localhost:8081/editevent", {
+    return axios.put(`${baseUrl}/editevent`, {
       edittedEvent,
       storeID,
     });
   },
 
   editProduct: (edittedProduct, storeID) => {
-    return axios.put("http://localhost:8081/editproduct", {
+    return axios.put(`${baseUrl}/editproduct`, {
       edittedProduct,
       storeID,
     });
   },
 
   deleteStore: (storeID) => {
-    return axios.delete("http://localhost:8081/deletestore", {
+    return axios.delete(`${baseUrl}/deletestore`, {
       data: { storeID },
     });
   },
 
   deleteEvent: (eventID, storeID) => {
-    return axios.delete("http://localhost:8081/deleteevent", {
+    return axios.delete(`${baseUrl}/deleteevent`, {
       data: { eventID, storeID },
     });
   },
 
   deleteProduct: (productID, storeID) => {
-    return axios.delete("http://localhost:8081/deleteproduct", {
+    return axios.delete(`${baseUrl}/deleteproduct`, {
       data: { productID, storeID },
     });
   },
 
   deleteImage: (imageToDelete, storeID) => {
-    return axios.delete("http://localhost:8081/deleteimage", {
+    return axios.delete(`${baseUrl}/deleteimage`, {
       data: { imageToDelete, storeID },
     });
   },
